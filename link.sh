f8056a813f8c2c698e237d798f31bd0b1c6b8404
princelai <princelailai@gmail.com>
2018-01-05 18:19:18 +0800

    add some files

---
+++ link.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ln -s .bashrc ~/
+ln -s .condarc ~/
+ln -s .gitconfig ~/
+ln -s .zshrc ~/
+ln -s .zpreztorc ~/
+ln -s spyder.desktop /usr/share/applications
+ln -s cmd-markdown.desktop /usr/share/applications
+
+ln -s pip.conf ~/.pip/
+ln -s matplotlibrc ~/.config/matplotlib/
+
+ln -s icons ~/.local/share/
+
