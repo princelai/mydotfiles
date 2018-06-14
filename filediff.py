1e4911d313b88abe724d595575579d87169f6e02
princelai <princelailai@gmail.com>
2018-06-14 18:28:37 +0800

    2018-06-14

--- filediff.py
+++ filediff.py
@@ -9,9 +9,9 @@ Created on Wed Jun 13 17:01:35 2018
 from pathlib import Path
 import subprocess
 
-
+# ~/.config/autostart/spyder.desktop
 files = ['~/.condarc', '~/.gitconfig', '~/.zshrc', '/usr/lib/prezto/runcoms/zpreztorc', 
-         '~/.config/autostart/spyder.desktop', '~/.pip/pip.conf',
+         '~/.local/share/applications/spyder.desktop', '~/.pip/pip.conf',
          '~/.config/matplotlib/matplotlibrc', '/etc/yaourtrc', '/etc/pacman.conf',
          '/etc/pacman.d/mirrorlist',]
 
@@ -23,9 +23,12 @@ for path in files:
     raw = p.expanduser()
     raw_exist = raw.exists()
     sync = p.cwd() / p.name
-    sync_exist = sync.exists()
+    # sync_exist = sync.exists()
     cmd = 'meld {} {}'.format(raw,sync)
-    if raw.owner() == 'root':
-        subprocess.run(f'sudo {cmd}',shell=True, executable='/usr/bin/zsh')
+    if raw_exist:        
+        if raw.owner() == 'root':
+            subprocess.run(f'sudo {cmd}',shell=True, executable='/usr/bin/zsh')
+        else:
+            subprocess.run(f'{cmd}',shell=True, executable='/usr/bin/zsh')
     else:
         subprocess.run(f'{cmd}',shell=True, executable='/usr/bin/zsh')
---
+++ restore_yaourt.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yaourt -S --needed - < yaourt.txt
